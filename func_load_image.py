deb3740d40df0754bf68aa4fdfe36de050c2ec9c
vaer <vaer07@gmail.com>
2024-01-08 01:05:59 +0300

    функция загрузки изображения в тех. целях

---
+++ func_load_image.py
@@ -0,0 +1,18 @@
+import pygame, os, sys
+
+
+def load_image(name, colorkey=None):#для удобной загрузки изображений
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:#чтобы фона не было
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
