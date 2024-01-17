a4e64a880b9168daab619d9172e71eb9f082f487
vaer <vaer07@gmail.com>
2024-01-17 12:17:11 +0300

    Png(все) + карта и камера

--- func_load_image.py
+++ func_load_image.py
@@ -2,7 +2,7 @@ import pygame, os, sys
 
 
 def load_image(name, colorkey=None):#для удобной загрузки изображений
-    fullname = os.path.join('data', name)
+    fullname = os.path.join('data/imgs', name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from menu_start import Menu, BasicMenu
+from menu_start import BasicMenu
 from func_load_image import load_image
 import pygame  # импорт библиотеки PyGame
-from PIL import Image
 
 pygame.init()  # инициализируем PyGame
 
 WIDTH = 1920  # ширина экрана
 HEIGHT = 1080  # высота экрана
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)  # создаем поверхность экрана
+screen = pygame.display.set_mode((WIDTH, HEIGHT))  # создаем поверхность экрана
 
-current_scene = None
+current_scene = print
 image = "zamok_gorod_fentezi_174584_1920x1080.jpg"
 
 
@@ -22,7 +21,7 @@ def switch_scene(scene):
 def setting_screen():
     menu = BasicMenu()
     menu.append_option('Играть', lambda: switch_scene(game_world_draw))
-    menu.append_option('Выбрать карту', lambda: quit)
+    menu.append_option('Выбрать размер', lambda: quit)
     menu.append_option('Вернуться', lambda: switch_scene(basic_menu_draw))
     running = True
     screen.blit(load_image(image), (0, 0))
@@ -48,10 +47,10 @@ def setting_screen():
         pygame.display.flip()
 
 
-def basic_menu_draw():
-    # global screen
-    # size = 1920, 1080
-    # screen = pygame.display.set_mode(size)
+def basic_menu_draw(*args):
+    global screen
+    size = 1920, 1080
+    screen = pygame.display.set_mode(size)
     menu = BasicMenu()
     menu.append_option('      1 VS 1      ', lambda: switch_scene(menu_draw))
     menu.append_option('Размер экрана', lambda: switch_scene(setting_screen))
@@ -80,7 +79,7 @@ def basic_menu_draw():
         pygame.display.flip()
 
 
-def menu_draw():
+def menu_draw(*args):
     size = 1920, 1080
     global screen
     screen = pygame.display.set_mode(size)
@@ -113,30 +112,32 @@ def menu_draw():
 
 
 def game_world_draw(*args):
-    img_objects_map = {'A': [load_image('data/imgs/hero.png')],
-                       'B': [load_image('data/imgs/abstract-blue-painting-acrylic-texture-with-marble-pattern.jpg')],
-                       '#': [load_image('data/imgs/kust.png')],
-                       '-': [load_image(
-                           'data/imgs/1626746394_9-kartinkin-com-p-pikselnaya-tekstura-travi-krasivo-11.jpg')],
-                       '*': [load_image('data/imgs/')],
-                       '@': [load_image('data/imgs/go_zamok.png')],
-                       '$': [load_image('data/imgs/go_zamok.png')],
-                       'G': [load_image('data/imgs/1677316897_foni-club-p-sunduk-piksel-art-1.png')],
-                       'W': [load_image('data/imgs/—Pngtree—wood log lumber pile cartoon_6955308.png')],
-                       'R': [load_image('data/imgs/')],
-                       'M': [load_image('data/imgs/kristal.jpg')],
-                       'O': [load_image('data/imgs/image0000.jpg')],
-                       'K': [load_image('data/imgs/b94adf7c5d21d13a83be3878336d0378.jpg')],
-                       'I': [load_image('data/imgs/')],
-                       'F': [load_image('data/imgs/gadalka.jpg')],
-                       '1': [load_image('data/imgs/as.jpg')],
-                       '2': [load_image('data/imgs/34174417_2210_w032_n002_608b_p15_608.jpg')],
-                       '3': [load_image('data/imgs/molot.jpg')],
-                       '4': [load_image('data/imgs/')],
-                       '5': [load_image('data/imgs/70015845_JEMA GER 1640-02.jpg')]
+    ret = args
+    map = ret[0]
+
+    img_objects_map = {'A': load_image('hero.png'),
+                       'B': load_image('hero.png'),
+                       '#': load_image('kust.png'),
+                       '-': load_image('1626746394_9-kartinkin-com-p-pikselnaya-tekstura-travi-krasivo-11.jpg'),
+                       '*': load_image('abstract-blue-painting-acrylic-texture-with-marble-pattern.jpg'),
+                       '@': load_image('go_zamok.png'),
+                       '$': load_image('go_zamok.png'),
+                       'G': load_image('1677316897_foni-club-p-sunduk-piksel-art-1.png'),
+                       'W': load_image('—Pngtree—wood log lumber pile cartoon_6955308.png'),
+                       'R': load_image('photo1705405793.png'),
+                       'M': load_image('kristal.png'),
+                       'O': load_image('image0000.png'),
+                       'K': load_image('b94adf7c5d21d13a83be3878336d0378.png'),
+                       'I': load_image('klipartz.com.png'),
+                       'F': load_image('gadalka.png'),
+                       '1': load_image('as.png'),
+                       '2': load_image('34174417_2210_w032_n002_608b_p15_608.png'),
+                       '3': load_image('molot.png'),
+                       '4': load_image('msg1331310743-41499.png'),
+                       '5': load_image('70015845_JEMA GER 1640-02.png')
                        }
-    size = [800, 800]
-    res = [400, 400]
+    size = [1000, 1000]
+    res = [500, 500]
 
     cam_x, cam_y = 0, 0
     global screen
@@ -148,13 +149,12 @@ def game_world_draw(*args):
     chunk_size = 8
     tile_size = 16
 
-    textures = {0: [pygame.image.load('0.png')],
-                1: [pygame.image.load('1.png')]}
+    r = open(f'{map}', mode="r").readlines()
+    map = [i.rstrip() + '#' * (chunk_size - len(map[0][:-3]) % chunk_size) for i in r]
+    map = map + ['#' * len(map[0])] * (chunk_size - len(map) % chunk_size)
 
-    world_size_chunk_x = 1024 // chunk_size
-    world_size_chunk_y = 1024 // chunk_size
-
-    world_map = Image.open('world.png').load()
+    world_size_chunk_x = len(map[0]) // chunk_size
+    world_size_chunk_y = len(map) // chunk_size
 
     def chunks_on_screen(cam, chunk_size, tile_size, res, world_size_chunk):
         x1 = cam[0] // (chunk_size * tile_size)
@@ -164,41 +164,45 @@ def game_world_draw(*args):
         y2 = (cam[1] + res[1]) // (chunk_size * tile_size)
 
         x1 = min(max(x1, 0), world_size_chunk[0] - 1)
-        x2 = min(max(x2, 0), world_size_chunk[1] - 1)
+        x2 = min(max(x2, 0), world_size_chunk[0] - 1)
 
-        y1 = min(max(y1, 0), world_size_chunk[0] - 1)
+        y1 = min(max(y1, 0), world_size_chunk[1] - 1)
         y2 = min(max(y2, 0), world_size_chunk[1] - 1)
 
         result = []
         for y in range(y1, y2 + 1):
             for x in range(x1, x2 + 1):
-                result.append(x + y * world_size_chunk[0])
+                result.append(x + y * world_size_chunk[1])
 
         return result
 
-    def generate_tile(x, y, chunk_x, chunk_y, w_map):
-        tile_x = (chunk_x // tile_size) + x
-        tile_y = (chunk_y // tile_size) + y
-
-        return int(x % 2 == 0)
-
     class Chunk:
-        def __init__(self, x, y, w_map):
+        trava = pygame.transform.scale(
+            load_image('1626746394_9-kartinkin-com-p-pikselnaya-tekstura-travi-krasivo-11.jpg'),
+            (tile_size, tile_size))
+
+        def __init__(self, coord_tiel, x, y):
+            self.n_tiel_x, self.n_tiel_y = coord_tiel
             self.x, self.y = x, y
-            self.map = [generate_tile(x, y, self.x, self.y, w_map) for y in range(chunk_size) for x in
-                        range(chunk_size)]
 
         def render(self):
             for y in range(chunk_size):
                 for x in range(chunk_size):
-                    texture = textures[self.map[x + y * chunk_size]][0]
-                    screen.blit(texture, (self.x + x * tile_size - cam_x, self.y + y * tile_size - cam_y))
+                    screen.blit(Chunk.trava, (self.x + x * tile_size - cam_x, self.y + y * tile_size - cam_y))
+                    if map[self.n_tiel_y + y][self.n_tiel_x + x - 1] in 'AB':
+                        pass
+                    else:
+                        texture = pygame.transform.scale(
+                            img_objects_map[map[self.n_tiel_y + y][self.n_tiel_x + x - 1]],
+                            (tile_size, tile_size))
+                        screen.blit(texture, (self.x + x * tile_size - cam_x, self.y + y * tile_size - cam_y))
 
     running = True
     chunks = []
     for y in range(world_size_chunk_y):
         for x in range(world_size_chunk_x):
-            chunks.append(Chunk(x * chunk_size * tile_size, y * chunk_size * tile_size, world_map))
+            chunks.append(
+                Chunk((x * chunk_size, y * chunk_size), x * chunk_size * tile_size, y * chunk_size * tile_size))
 
     frame = 0
     while running:
@@ -214,18 +218,19 @@ def game_world_draw(*args):
 
         key = pygame.key.get_pressed()
         if key[pygame.K_a]:
-            cam_x -= 1
+            cam_x -= 3
         if key[pygame.K_d]:
-            cam_x += 1
+            cam_x += 3
         if key[pygame.K_w]:
-            cam_y -= 1
+            cam_y -= 3
         if key[pygame.K_s]:
-            cam_y += 1
+            cam_y += 3
 
         for i in chunks_on_screen((cam_x, cam_y), chunk_size, tile_size, res, (world_size_chunk_x, world_size_chunk_y)):
             chunks[i].render()
 
         window.blit(pygame.transform.scale(screen, size), (0, 0))
+        screen.blit(pygame.transform.scale(window, size), (0, 0))
         pygame.display.update()
         clock.tick(480)
 
@@ -233,8 +238,11 @@ def game_world_draw(*args):
         if frame % 100 == 0:
             pygame.display.set_caption('FPS: ' + str(round(clock.get_fps())))
             chunks_on_screen((cam_x, cam_y), chunk_size, tile_size, res, (world_size_chunk_x, world_size_chunk_y))
+    return ret
 
-    switch_scene(basic_menu_draw)
-    while current_scene is not None:
-        current_scene()
-    pygame.quit()
+
+switch_scene(game_world_draw)
+data_game = 'data/maps/map_1.txt'
+while current_scene is not None:
+    data_game = current_scene(data_game)
+pygame.quit()
